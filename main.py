6cca644a38cb6de00a192efef544c3f6ef9fde67
Sinsangwoo <aksrkd7191@gmail.com>
2025-12-16 21:49:14 +0900

    초기 작업 완료

---
+++ main.py
@@ -0,0 +1,9 @@
+from process import Process
+
+def main():
+    print("---미니 프로세스 시뮬레이터 시작---")
+    p1 = Process()
+    print("---미니 프로세스 시뮬레이터 종료---")
+
+if __name__ == "__main__":
+    main()
---
+++ process.py
@@ -0,0 +1,4 @@
+class Process:
+    def __init__(self) :
+        print("✨ 새로운 프로세스가 생성되었습니다!")
+        pass
