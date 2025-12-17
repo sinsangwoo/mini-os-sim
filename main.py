b3e8f24c165709469a633fda590112e5a1b0b415
Sinsangwoo <aksrkd7191@gmail.com>
2025-12-17 15:55:59 +0900

    feat: PID, 도착시간, 실행시간  기능 추가. main.py에 p1 p2 p3 프로세스 만들어서 시험해봄

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ from process import Process
 
 def main():
     print("---미니 프로세스 시뮬레이터 시작---")
-    p1 = Process()
+    
+    #모의 시나리오
+
+    #1. 1번 프로세스: 0초에 도착, 3초 동안 실행 
+    p1 = Process(arrival_time=0, burst_time=3)
+
+    #2. 2번 프로세스: 1초에 도착, 5초 동안 실행
+    p2 = Process(arrival_time=1, burst_time=5)
+    
+    #3. 3번 프로세스: 2초에 도착, 2초 동안 실행 
+    p3 = Process(arrival_time=2, burst_time=2)
+
+    #4. 각 프로세스들의 PID 출력
+    print("\n[메모리 확인]")
+    print(f"P1의 PID: {p1.pid}")  
+    print(f"P2의 PID: {p2.pid}")  
+    print(f"P3의 PID: {p3.pid}")  
+
     print("---미니 프로세스 시뮬레이터 종료---")
 
 if __name__ == "__main__":
--- process.py
+++ process.py
@@ -1,4 +1,16 @@
 class Process:
-    def __init__(self) :
-        print("✨ 새로운 프로세스가 생성되었습니다!")
-        pass
+    _pid_counter = 1  # 클래스 변수로 프로세스 ID를 관리
+    def __init__(self, arrival_time, burst_time): #arrvial_time = 프로세스가 도착한 시간, burst_time = 프로세스가 CPU를 점유하는 시간
+        
+        #1. 프로세스가 생성될 때마다 고유한 프로세스 ID를 부여함
+        self.pid = Process._pid_counter
+
+        #2. 다음 프로세스를 위해 프로세스 ID 카운터를 증가시킴
+        Process._pid_counter += 1
+
+        #3. 프로세스의 도착 시간과 실행 시간을 설정함
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+
+        #4. 프로세스 생성 메시지를 출력함
+        print(f"✨ [PID: {self.pid}] 프로세스 생성! (도착: {arrival_time}, 실행시간: {burst_time})")
